0e62ec2999aeef31ac8ffc95e104a2acb4990c09
Mayank Sharma <mayankshar21@yahoo.com.sg>
2020-08-30 16:56:30 +1000

    Updated node app

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('<h1>Mayank Server</h1>');
+    }
+});
+
+server.listen(PORT, HOSTNAME, () => console.log(`Server running at http://${HOSTNAME}:${PORT}/`));
